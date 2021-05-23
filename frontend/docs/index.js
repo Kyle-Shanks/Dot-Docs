d2a05c84f380ad9a52a594ffe9286a6f70e7cd17
KJ Shanks <kyle.j.shanks@gmail.com>
2021-05-23 14:58:43 -0400

    Add docs for v0.2.1

--- frontend/docs/index.js
+++ frontend/docs/index.js
@@ -1,5 +1,5 @@
-export { default as componentsData } from './v0.2.0/componentsData.json';
-export { default as coreData } from './v0.2.0/coreData.json';
-export { default as effectsData } from './v0.2.0/effectsData.json';
-export { default as instrumentsData } from './v0.2.0/instrumentsData.json';
-export { default as sourcesData } from './v0.2.0/sourcesData.json';
+export { default as componentsData } from './v0.2.1/componentsData.json';
+export { default as coreData } from './v0.2.1/coreData.json';
+export { default as effectsData } from './v0.2.1/effectsData.json';
+export { default as instrumentsData } from './v0.2.1/instrumentsData.json';
+export { default as sourcesData } from './v0.2.1/sourcesData.json';
